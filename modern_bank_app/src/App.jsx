58c817dcf7d806caf068262a1f9203fe465f15c6
markohautala <markohautala@live.se>
2024-07-08 19:14:27 +0200

    Update App component with NavBar, Hero Section, and Stats Section

--- modern_bank_app/src/App.jsx
+++ modern_bank_app/src/App.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import styles from './style'
 
-  const App = () => (
-      <div className="bg-primary w-full overflow-hidden">
-        <h1>Modern Bank App</h1>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+
+    {/* NavBar goes here */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        NavBar goes here
       </div>
-    )
+    </div>
+
+    {/* Hero Section goes here */}
+    <div className={` bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero Section goes here
+      </div>
+    </div>
+
+    {/* Stats Section goes here */}
+    <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Cliens
+        CTA Section
+        Footer
+        
+      </div>
+    </div>
+  </div>
+)
 
 export default App
