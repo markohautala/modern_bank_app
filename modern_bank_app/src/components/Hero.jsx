e4fac6529deda95b048cf20c9318a19fccffc8aa
markohautala <markohautala@live.se>
2024-07-27 18:08:56 +0200

    Add big-hero-text and add round get-started icon

--- modern_bank_app/src/components/Hero.jsx
+++ modern_bank_app/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { discount, robot } from '../assets'
+import GetStarted from './GetStarted'
 
 
 const Hero = () => {
@@ -15,6 +16,20 @@ const Hero = () => {
             <span className='text-white'>For 1 Month </span>
             Subscription
           </p>
+        </div>
+
+        <div className='flex flex-row justify-between items-center w-full'>
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+            The Next
+            <br className='sm:block hidden'/> {" "}
+            <span className='text-gradient'>Generation</span> {" "}
+            Payment Method.
+          </h1>
+
+          <div className='ss:flex hidden md:mr-4 mr-0'>
+            <GetStarted />
+          </div>
+
         </div>
       </div>
 
